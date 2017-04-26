db64e3ad2680f1b744d17af7877861f110ece8b3
yarob <yarob.al-taay@union.co.uk>
2017-04-26 14:28:19 +0100

    implement clickable Events

--- src/Components/EventCard.vue
+++ src/Components/EventCard.vue
@@ -1,17 +1,48 @@
 <template>
-    <div class="panel no-margin" :class="[event.color]">
-        <div class="panel-heading event-title">{{event.title}}</div>
+    <div class="panel no-margin" :class="[event.color]" @click="showEventDetails">
+        <div class="panel-heading event-title" :class="{'clickable-event':clickableEvent}">{{event.title}}</div>
     </div>
 </template>
 
 <script>
+    import {DAY_SELECTED} from '../actions';
     export default {
-        props: ['event', 'date', 'firstDay']
+        data(){
+            return {
+                clickableEvent:false
+            }
+        },
+        props: {
+            event: {
+                type: Object
+            }
+        },
+        created(){
+            let me = this;
+            EventsBus.$on(DAY_SELECTED, function (selectedDay) {
+                if(me.event.date.isSame(selectedDay.dayDate, 'day')){
+                    me.clickableEvent = true;
+                }
+                else {
+                    me.clickableEvent = false;
+                }
+            })
+        },
+        methods: {
+            showEventDetails() {
+                if(this.clickableEvent){
+                    alert(this.event.title+' is selected. Will leave the implementation to you too :)');
+                }
+            }
+        }
     }
 </script>
 <style>
     .event-title {
         padding: 0px 5px;
         font-size: 12px
+    }
+    .clickable-event {
+        text-decoration: underline;
     }
 </style>
